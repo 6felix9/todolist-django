3b8ef1546e8d516e3f8c59d56282c503095acd68
felix <puahtzefoong@gmail.com>
2025-05-13 18:37:53 +0800

    Minor fix

--- ToDoList_backend/build.sh
+++ ToDoList_backend/build.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-cd ToDoList
+
+# 1) Build frontend (one level up)
+cd ../ToDoList
 npm install
 npm run build
 
+# 2) Back into backend
 cd ../ToDoList_backend
 pip install -r requirements.txt
 
-mkdir -p templates/frontend
-mkdir -p static/frontend/assets
-
+# 3) Copy assets
+mkdir -p templates/frontend static/frontend/assets
 cp ../ToDoList/dist/index.html templates/frontend/index.html
 cp -r ../ToDoList/dist/assets/* static/frontend/assets/
 
+# 4) Django steps
 python manage.py collectstatic --noinput
 python manage.py migrate
